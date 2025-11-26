3a7c12c0751af7c2006d808befa700d9f364c3cb
Pieter deVries <pieterdevries@google.com>
2025-11-26 17:07:10 +0900

    Fix App Engine deployment issues

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { lintLiquid } from './linter.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
